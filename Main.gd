883bc0fe4033d94c6e222cb981244b65f8c5d084
Oleg Yurchenko <oleg@bgmot.com>
2021-05-26 12:42:08 -0400

    Added a SloMeter slider, and functionality, with reload time

--- Main.gd
+++ Main.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
 export var gSpeedModifier = 1
+var SloMoCharge = 100
+var sinceLastUse
+
+func _ready():
+	sinceLastUse = 2
 
 func _process(delta):
-	if(Input.is_action_pressed("slowmode")):
+	if(Input.is_action_pressed("slowmode") and SloMoCharge > 0):
+		sinceLastUse = 0
 		gSpeedModifier = 0.25
 	else:
 		gSpeedModifier = 1
+	if(gSpeedModifier == 0.25):
+		SloMoCharge -= 1
+	else:
+		sinceLastUse += delta
+	if(sinceLastUse >= 1.5):
+		SloMoCharge += 0.5
+	if(SloMoCharge > 100):
+		SloMoCharge = 100
+	$CanvasLayer/ProgressBar.value = SloMoCharge
